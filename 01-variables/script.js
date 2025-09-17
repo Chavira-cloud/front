fb1852b7f207fd458727a4452b19b88025d2a8b4
Chavira-cloud <markos.mgcg@gmail.com>
2025-09-17 16:58:31 -0700

    Inicio de js es6

---
+++ 01-variables/script.js
@@ -0,0 +1,13 @@
+let nombre = 'Marco';
+let direccion = 'dominic';
+const rfc= '21323';
+console.log(nombre);
+
+function detalles(){
+    let nombre = 'Marco';
+    let direccion = 'dominic, 123';
+    const rfc= '21323';
+    console.log(direccion)
+}
+
+detalles();
