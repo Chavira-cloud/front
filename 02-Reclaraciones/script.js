14207a120d8c1530ed12188735073b9e42d0b61c
Chavira-cloud <markos.mcgc@gmail.com>
2025-09-18 07:43:32 -0700

    Avance de js es6

---
+++ 02-Reclaraciones/script.js
@@ -0,0 +1,13 @@
+var nombre = 'David';
+var nombre = 'David';
+console.log(nombre);
+
+/*
+    let direccion = 'Calle falsa, 123';
+    let direccion = 'Nueva direccion';
+    console.log(direccion);
+*/
+
+const rfc = 'ewrv32f';
+const rfc = '3432jew';
+console.log(rfc);
---
+++ 04-Reasignar/script.js
@@ -0,0 +1,19 @@
+const nombre = 'David';
+
+const perro ={
+    nombre: 'Mel',
+    edad: 10
+}
+
+/*
+    perro = {
+        nombre: 'nua',
+        edad: 5
+    }
+*/
+
+perro.nombre = 'nua'; // Aqui se cambia las propiedades del objeto
+
+perro.patita = true;
+
+console.log(perro);
