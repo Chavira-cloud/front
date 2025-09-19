0efcb7316b66f6f681d588040e51eec343de1f60
Chavira-cloud <markos.mcgc@gmail.com>
2025-09-18 22:20:37 -0700

    Agregacion de eventos de mouse

--- JsIntermedio/07-ProblemaCofla.js
+++ JsIntermedio/07-ProblemaCofla.js
@@ -12,7 +12,34 @@ function saludar(){
     alert("Pedro")
 } ); */
 
+//Bubbling y Capturing
+/* const button = document.querySelector(".button");
+const contenedor1= document.querySelector(".contenedor-1");
+const contenedor2= document.querySelector(".contenedor-2");
+
+contenedor1.addEventListener("click", (e)=>{
+    alert("di click en el contenedor rojo");
+    e.stopPropagation(); // para que  no se propague
+});
+ 
+contenedor2.addEventListener("click", (e)=>{
+    alert("di click en el contenedor azul");
+}); */
+
+//Mouse Events
 const button = document.querySelector(".button");
-button.addEventListener("click", (e)=>{
-    console.log(e.target);
+const contenedor1= document.querySelector(".contenedor-1");
+const contenedor2= document.querySelector(".contenedor-2");
+
+// button.addEventListener("dblclick", (e)=>{
+//     alert("di click en un boton");
+// });
+
+contenedor1.addEventListener("mouseover", (e)=>{
+    alert("di click en el contenedor rojo");
+});
+
+
+contenedor2.addEventListener("contextmenu", (e)=>{
+    alert("di click en el contenedor azul");
 });
