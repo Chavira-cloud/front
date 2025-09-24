ba5e55a8639ca70b4dd4ae772e2cc79a242c51f2
Chavira-cloud <markos.mcgc@gmail.com>
2025-09-23 22:50:47 -0700

    Avance cap 10 swich

--- JsIntermedio/capitulo 10/scrit.js
+++ JsIntermedio/capitulo 10/scrit.js
@@ -9,3 +9,20 @@ if (3 > 3){ //Sentencia de control
     let nombre = "Marco"
     alert(nombre)
 } */
+
+
+let expr = "Manzana";    
+switch (expr) {
+    case "Banana":
+        console.log("Esta fruta es amarrilla");
+        break;
+    case "Pera":
+        console.log("Tienes forma de tio roberto");
+    break;
+    case "Manzana":
+        console.log("Los primeros humanos la comieron");
+    break;
+    default:
+        console.log("No es ninguna");
+    break;
+}
