567ac062d5a769192ba531ee9b09c2232cf4873e
Chavira-cloud <markos.mgcg@gmail.com>
2025-09-25 17:01:52 -0700

    Avance problema cofla

--- JsIntermedio/capitulo 10/Problema/script.js
+++ JsIntermedio/capitulo 10/Problema/script.js
@@ -3,11 +3,12 @@ const sendButton = document.getElementById('snd-nota');
 sendButton.addEventListener("click", ()=>{
     let resultado, mensaje;
     try{
-        resultado = parseInt(document.getElementById('nota').value);
-        if(isNaN(resultado)){
+        prevRes = parseInt(document.getElementById('nota').value);
+        if(isNaN(prevRes)){
             throw "Gracioso";
         }
-        mensaje ="Hola";
+        mensaje = definirMensaje(prevRes);
+        resultado = verificarAprovacion(8,5,prevRes);
     }catch(e){
         resultado = "Eres gracioso?";
         mensaje = "He descubierto que intentaste hackear el sitio";
@@ -16,7 +17,47 @@ sendButton.addEventListener("click", ()=>{
 });
 
 
+const definirMensaje = (pr)=>{
+    let resultado;
+    switch(pr){
+        case 1 : resultado = "No puede ser HDP";
+        break;
+        case 2 : resultado = "eres malicimo par mi materia";
+        break;
+        case 3 : resultado = "No sabes casi nada";
+        break;
+        case 4 : resultado = "muy mal";
+        break;
+        case 5 : resultado = "mal";
+        break;
+        case 6 : resultado = "Regular";
+        break;
+        case 7 : resultado = "bien , pero puede mejorar";
+        break;
+        case 8 : resultado = "!muy bien!";
+        break;
+        case 9 : resultado = "Excelente";
+        break;
+        case 10 : resultado = "Insuperable hijo de su pinshi madre";
+        break;
+        default: resultado = null; 
+    }
+    return resultado;
+}
+
+const verificarAprovacion = (nota1, nota2,prevRes) =>{
+    promedio = (nota1 + nota2 + prevRes) / 3;
+    if (promedio >=  7){
+        return `<span class='green'>APROBADO</span>`;
+    }else{
+         return `<span class='red'>NO APROBADO</span>`;
+    }
+}
+
 const abrirModal = (res, msg)=>{
-    console.log(res);
-    console.log(msg);
+    document.querySelector(".resultado").innerHTML = res;
+    document.querySelector(".mensaje").innerHTML = msg;
+    let modal = document.querySelector(".modal-background");
+    modal.style.display= "flex";
+    modal.style.animation = "aparecer 1s forwards";
 }
